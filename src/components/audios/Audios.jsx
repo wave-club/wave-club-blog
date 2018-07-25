8628af523237332e34d2d729ca01cd929aa16a98
2yuechanghe <liujunbingdea3@163.com>
2018-07-26 02:32:02 +0800

    add source

--- src/components/audios/Audios.jsx
+++ src/components/audios/Audios.jsx
@@ -2,6 +2,12 @@ import './Audios.scss'
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import AudiosList from './audios-list/AudiosList'
+import {Card} from 'antd'
+
+const gridStyle = {
+    width: '33%',
+    textAlign: 'center',
+}
 
 class Audios extends Component {
 
@@ -11,13 +17,76 @@ class Audios extends Component {
 
     componentDidMount() {
 
+        var onOff=true;
+        $(".btn").click(function(){
+            if(onOff)
+            {
+                $(".btn").get(0).src="img/pause.png";
+                $("#myMusic").get(0).play();
+                $(".run").addClass("rotate");
+                $(".star").addClass("rotate");
+            }
+            else
+            {
+                $(".btn").get(0).src="img/play.png";
+                $("#myMusic").get(0).pause();
+                $(".run").removeClass("rotate");
+                $(".star").removeClass("rotate");
+            }
+            onOff=!onOff;
+        });
+        var na=new Array();//歌手
+        na[0]="陈淑桦 ";
+        na[1]="周杰伦";
+        na[2]="李行亮";
+        var ng=new Array();//歌名
+        ng[0]="笑红尘";
+        ng[1]="青花瓷";
+        ng[2]="愿得一人心";
+        var n=0;
+        $(".next").click(function()
+        {
+            n++;
+            if(n>2)
+            {
+                n=0;
+            }
+            $("#myMusic").attr('src','music/'+n+'.mp3');
+            $(".star").attr('src','img/'+n+'.jpg');
+            $(".singer").html(na[n]);
+            $(".song").html(ng[n]);
+            $(".btn").get(0).src="img/pause.png";
+            $("#myMusic").get(0).play();
+            $(".run").addClass("rotate");
+            $(".star").addClass("rotate");
+            onOff=false;
+        });
+        $(".love").click(function()
+        {
+            alert("感谢支持！");
+        })
+
     }
 
 
     render() {
         return (
             <div className="audios">
-                <AudiosList/>
+                <div id="box">
+                    <div className="circle">
+                        <img src="img/yyjd.png" className="run"/>
+                        <p className="singer">陈淑桦</p>
+                        <p className="song">笑红尘</p>
+                        <div>
+                            <img src="img/0.jpg" className="star" height="120px" width="120px" alt="陈淑桦"/>
+                            <img src="img/play.png" className="btn" alt="开始" />
+                        </div>
+                        <p className="love">好听</p>
+                        <p className="next">切歌</p>
+                    </div>
+                </div>
+                <audio src='../../fetopic-assets/audios/0.mp3' id="myMusic" />
+
             </div>
         )
     }
