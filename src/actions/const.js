8be44b87d4bbe4fbc6ccfb7cb2b7a3abe7435f0f
2yuechanghe <liujunbingdea3@163.com>
2018-07-26 05:08:54 +0800

    update .gitignore

--- src/actions/const.js
+++
@@ -1,9 +0,0 @@
-const Actions = {
-    SHOWLOADING: 'SHOWLOADING',
-    HIDELOADING: 'HIDELOADING',
-    LOADING: 'LOADING',
-    POSTLOGIN: 'POSTLOGIN',
-    SIDE_BAR: 'SIDE_BAR'
-}
-
-export default Actions
--- src/i18n/zh_cn.js
+++
@@ -1,9 +0,0 @@
-/**
- * @class APP 中文 , 为双语做架构
- *
- **/
-
-const Lan = {
-
-}
-export default Lan
