1a63ca5bf236341e9bed697b38170603aea3f34f
2yuechanghe <liujunbingdea3@163.com>
2018-08-08 10:10:39 +0800

    洗牌算法 doc

--- extend/Array.js
+++ extend/Array.js
@@ -1,4 +1,5 @@
 /**
+ * @link  https://github.com/976500133/FETopic/issues/68
  * @descrption 随机数 洗牌 Fisher–Yates shuffle
  * **使用方式 [1,2,3,4,5,6,7,8].shuffle()
  * **返回值 [4, 6, 3, 2, 5, 1, 7, 8] // 每次结果都是随机的
